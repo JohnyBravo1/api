78067914d5fc32e3661f56799105fa8c490289db
Johan Boonzaier <johanboo@bcx.co.za>
2018-09-25 09:14:18 +0200

    Added regions api call

--- application/controllers/Json.php
+++ application/controllers/Json.php
@@ -17,16 +17,66 @@ class Json extends CI_Controller {
 
     private static $_PROVINCES = [
 
-        "Eastern Cape",
-        "Free State",
-        "Gauteng",
-        "Kwazulu-Natal",
-        "Limpopo",
-        "Mpumalanga",
-        "Northern Cape",
-        "North West",
-        "Polokwane",
-        "Western Cape",
+        "Eastern Cape" => [
+
+            "Grahamstown",
+            "Aliwal",
+            "Rhodes",
+            "Bisho",
+            "Port Elizabeth"
+        ],
+        "Free State" => [
+
+            "Bloemfontein",
+            "Parys",
+            "Welkom"
+        ],
+        "Gauteng" => [
+
+            "Pretoria",
+            "Johannesburg",
+            "Cullinan"
+        ],
+        "Kwazulu-Natal" => [
+
+            "Durban",
+            "Umhlanga",
+            "Richards Bay",
+            "Scottburgh"
+        ],
+        "Limpopo" => [
+
+            "Louis Trichardt",
+            "Polokwane"
+        ],
+        "Mpumalanga" => [
+
+            "Waterval Boven",
+            "Nelspruit",
+            "Badplaas",
+            "Sabie",
+            "Waterberg"
+        ],
+        "Northern Cape" => [
+
+            "Augrabies",
+            "Port nolloth",
+            "Kimberley"
+        ],
+        "North West" => [
+
+            "Rustenburg",
+            "Hartbeespoort",
+            "Britz",
+            "Pilansberg"
+        ],
+        "Western Cape" => [
+
+            "Stellenbosch",
+            "Gordons Bay",
+            "Cape town",
+            "Langebaan"
+        ],
     ];
 
     public function __construct() {
@@ -45,22 +95,68 @@ class Json extends CI_Controller {
             $output = $this->_shuffled();
         }
 
-        $this->_normalize($output);
+        $this->_normalizeCities($output);
+
+        $output = json_encode($output);
 
         $this->output->set_content_type("application/json");
-        $this->output->set_output(json_encode($output));
+        $this->output->set_output($output);
     }
 
-    private function _normalize(&$cities) {
+    public function regions() {
 
+        $result = NULL;
+
+        if ($this->input->post() != NULL) {
+
+            $province = $this->input->post("province");
+
+            foreach (Json::$_PROVINCES as $provinceName => $cities) {
+
+                if (stristr($province, $provinceName) != NULL) {
+
+                    $result[$provinceName] = $cities;
+                    break;
+                }
+            }
+        }
+        else {
+            
+            $result = JSON::$_PROVINCES;
+        }
+
+        $this->_normalizeRegions($result);
+        $output = json_encode($result);
+
+        $this->output->set_content_type("application/json");
+        $this->output->set_output($output);
+    }
+
+    private function _normalizeCities(&$cities)
+    {
         $result = [];
 
         foreach ($cities as $city) {
 
             $result[]['name'] = $city;
         }
+        $cities = $result;
+    }
 
-        // $cities = $result;
+    private function _normalizeRegions(&$regions) {
+
+        $result = [];
+        $regionIndex = 0;
+        foreach($regions as $regionName => $cities) {
+
+            $this->_normalizeCities($cities);
+            
+            $result['province'][$regionIndex]['city'] = $cities;
+            $result['province'][$regionIndex]['name'] = $regionName;
+
+            $regionIndex++;
+        }
+        $regions = $result;
     }
 
     private function _shuffled() {
